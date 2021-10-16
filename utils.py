74b498cea89a2671db4e6609ca993c38dabe76c7
rgee3894 <rgga16@gmail.com>
2021-10-16 18:12:16 +0800

    minor changes

--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
-import torchvision 
+import torch
 from torchvision import transforms
 from PIL import Image
-import numpy as np
-import os 
 
 # Loads a single image
 def load_image(filename):
@@ -12,7 +9,7 @@ def load_image(filename):
 
 
 # Preprocesses image and converts it to a Tensor. Resizes image if there is a specified size.
-def image_to_tensor(image,device,resize=None):
+def image_to_tensor(image,device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),resize=None):
 
     if resize is not None:
         if type(resize) is not tuple:
